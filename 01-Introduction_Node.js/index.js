d3b30f9308bdfad62649e4f40cce1907bcb88af6
Akash More <akashkmore1@gmail.com>
2022-08-25 12:07:08 +0530

    Learn about fs(sile system) module

---
+++ 01-Introduction_Node.js/index.js
@@ -0,0 +1,5 @@
+// fs stands for file system.
+// fs module used for reading and writing data from a file.
+// require(fs) will return an object of lot of function.
+// This object we are directly assigning to fs variable
+const fs = require("fs");
