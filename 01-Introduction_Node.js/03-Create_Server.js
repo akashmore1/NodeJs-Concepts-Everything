c8bef9f472617b90b37290c1570abb346601aa52
Akash More <akashkmore1@gmail.com>
2022-08-27 22:24:40 +0530

    Ports in node js

--- 01-Introduction_Node.js/03-Create_Server.js
+++ 01-Introduction_Node.js/03-Create_Server.js
@@ -6,6 +6,7 @@ const server = http.createServer((req, res) => {
   res.end("Hello From Server!");
 });
 
+// here 8000 is port number, we generally use 8000 port number for node js
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to server on port 8000");
 });
