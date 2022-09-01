f57f4642c58963fe95f3190dbc56a41e96d1fbe8
Akash More <akashkmore1@gmail.com>
2022-09-01 20:41:35 +0530

    Add simple routes in node project

--- 01-Introduction_Node.js/04-Routing.js
+++ 01-Introduction_Node.js/04-Routing.js
@@ -1,4 +1,22 @@
-const http = require("http");
-
 // In bigger applications routing can be very complex.
 // So we use a tool for it called 'Express'
+// For now we will do everything from scrach
+
+const http = require("http");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/overview") {
+    res.end("This is from overview");
+  } else if (pathName === "/product") {
+    res.end("This is from product");
+  } else {
+    res.end("Hello from server");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to server at port 8000");
+});
