2443b912fe8819d3d63a57fb0437b4f5475a38d0
Akash More <akashkmore1@gmail.com>
2022-09-10 23:45:43 +0530

    Learn about pipe operator in streams

--- 02-How_Backend_Works/03-Streams.js
+++ 02-How_Backend_Works/03-Streams.js
@@ -9,21 +9,26 @@ server.on("request", (req, res) => {
   //   });
   // =============================================================== //
   // Solution 2
+  // const readable = fs.createReadStream("./files/test-file.txt");
+  // readable.on("data", (chunk) => {
+  //   res.write(chunk);
+  // });
+  // readable.on("end", () => {
+  //   res.end();
+  // });
+  // readable.on("error", (error) => {
+  //   console.log(error);
+  //   res.statusCode = 500;
+  //   res.end("File not found");
+  // });
+  // 1. Readable stream is much faster than actually sending the result with response writable stream in network.
+  // 2. This will overwhelm the response stream, which cannot handle all this coming data superfast.
+  // 3. This problem is called as Back Pressure.
+  // So here is a better solution that will resolve back pressure
+  // Solution 3
 
   const readable = fs.createReadStream("./files/test-file.txt");
-  readable.on("data", (chunk) => {
-    res.write(chunk);
-  });
-
-  readable.on("end", () => {
-    res.end();
-  });
-
-  readable.on("error", (error) => {
-    console.log(error);
-    res.statusCode = 500;
-    res.end("File not found");
-  });
+  readable.pipe(res);
 });
 
 server.listen(8000, "127.0.0.1", () => {
