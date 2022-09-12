4c05f053b8deae6c3412e6b07e09c58f39007719
Akash More <akashkmore1@gmail.com>
2022-09-12 19:35:13 +0530

    Promisify readFile function

--- 04-Asynchronous_JS/index.js
+++ 04-Asynchronous_JS/index.js
@@ -30,3 +30,18 @@ fs.readFile("./dog.txt", (err, data) => {
       throw new Error(err);
     });
 });
+
+// Make reading and writing files through promises
+
+const readFilePro = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+};
+
+readFilePro("./dog.txt").then((res) => {
+  superagent.get(`https://dog.ceo/api/breed/${res}/images/random`);
+});
