30feaa951aa410e6d75dc60e4363fa8b1641e398
Akash More <akashkmore1@gmail.com>
2022-09-12 19:21:00 +0530

    Use promise to call dog api

--- 04-Asynchronous_JS/index.js
+++ 04-Asynchronous_JS/index.js
@@ -2,18 +2,31 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
+// // With callback hell
+// fs.readFile("./dog.txt", (err, data) => {
+//   console.log(`Breed = ${data}`);
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .end((err, res) => {
+//       if (err) return console.log(err.message);
+//       console.log(res.body.message);
+
+//       // Create a new file and save this img url
+//       fs.writeFile("./dog-img.txt", res.body.message, (err) => {
+//         if (err) return console.log(err.message);
+//         console.log("Random image saved to file");
+//       });
+//     });
+// });
+
+// With Promises
 fs.readFile("./dog.txt", (err, data) => {
-  console.log(`Breed = ${data}`);
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-      console.log(res.body.message);
-
-      // Create a new file and save this img url
-      fs.writeFile("./dog-img.txt", res.body.message, (err) => {
-        if (err) return console.log(err.message);
-        console.log("Random image saved to file");
-      });
+    .then((res) => {
+      return res.body.message;
+    })
+    .catch((err) => {
+      throw new Error(err);
     });
 });
