bc7663d67b2cdfb4dd4c2879ceab14d0368b5f77
Akash More <akashkmore1@gmail.com>
2022-09-16 19:44:35 +0530

    Simple post request in express

--- 05-Express_Start_Building_API/app.js
+++ 05-Express_Start_Building_API/app.js
@@ -8,6 +8,12 @@ app.get('/', (req, res) => {
   res.status(200).json({ msg: 'Hello from server!', app: 'Natours' });
 });
 
+app.post('/', (req, res) => {
+  res.status(200).send('You can write to this request');
+});
+
+// We don't need to specify content-type in experss it takes care of headers for us
+
 app.listen(port, () => {
   console.log(`App started running at ${port}`);
 });
