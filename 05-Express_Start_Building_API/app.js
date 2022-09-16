025fa8c201fddab3398902906472fb7c8581b7f4
Akash More <akashkmore1@gmail.com>
2022-09-16 19:40:23 +0530

    Basic Routing in express

--- 05-Express_Start_Building_API/app.js
+++ 05-Express_Start_Building_API/app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from server side!');
+  res.status(200).json({ msg: 'Hello from server!', app: 'Natours' });
 });
 
 app.listen(port, () => {
