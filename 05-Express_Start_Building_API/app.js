8c2a0592e6cd6dc2bf52d76543e5e9cc44a508b0
Akash More <akashkmore1@gmail.com>
2022-09-24 22:41:05 +0530

    Create a get tours api in express

--- 05-Express_Start_Building_API/app.js
+++ 05-Express_Start_Building_API/app.js
@@ -1,21 +1,36 @@
 // For running this application 'npx nodemon [your-app.js]'
 
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ msg: 'Hello from server!', app: 'Natours' });
-});
+// Convert into javsscript object
+const tours = JSON.parse(
+  fs.readFileSync('./dev-data/data/tours-simple.json', 'utf-8')
+);
 
-app.post('/', (req, res) => {
-  res.status(200).send('You can write to this request');
+// Build natours api
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    ok: true,
+    result: tours.length,
+    data: { tours },
+  });
 });
 
 // We don't need to specify content-type in experss it takes care of headers for us
-
 app.listen(port, () => {
   console.log(`App started running at ${port}`);
 });
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({ msg: 'Hello from server!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(200).send('You can write to this request');
+// });
