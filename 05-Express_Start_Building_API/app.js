63440e95641d6b99b4fdba67766922ff1d7f5613
Akash More <akashkmore1@gmail.com>
2022-09-24 23:16:37 +0530

    Create a post tours api in express

--- 05-Express_Start_Building_API/app.js
+++ 05-Express_Start_Building_API/app.js
@@ -5,6 +5,9 @@ const express = require('express');
 
 const app = express();
 
+// middleware
+app.use(express.json());
+
 const port = 3000;
 
 // Convert into javsscript object
@@ -12,7 +15,7 @@ const tours = JSON.parse(
   fs.readFileSync('./dev-data/data/tours-simple.json', 'utf-8')
 );
 
-// Build natours api
+// Build natours get api
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -20,6 +23,34 @@ app.get('/api/v1/tours', (req, res) => {
     result: tours.length,
     data: { tours },
   });
+});
+
+// Build natours post api
+// In post request, we ca send data from client to server
+// 'req' parameter in callback of post request will have that client/user's data.
+// But express doen not make this data directly available in req
+// In order to make that data available, we use middleware.
+// app.use(express.json()); is used to make middleware available
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    './dev-data/data/tours-simple.json',
+    JSON.stringify(tours),
+    (err) => {
+      if (err) res.send(err);
+      res.status(201).json({
+        status: 'success',
+        ok: true,
+        result: tours.length,
+        data: newTour,
+      });
+    }
+  );
 });
 
 // We don't need to specify content-type in experss it takes care of headers for us
